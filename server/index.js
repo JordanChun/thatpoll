82b9ea3469d6b01f63b9c6467cffb63eada6ed24
Jordan <jordanchunxd@gmail.com>
2019-11-03 12:46:58 -0800

    ip server whitelist test

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const requestIp = require('request-ip');
 const ipfilter = require('express-ipfilter').IpFilter
 
 // Blacklist the following IPs
-const ips = ['127.0.0.1'];
+const ips = ['54.165.34.193:443'];
 
 const mongoose = require('mongoose');
 
